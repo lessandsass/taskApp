0ecd0cc62ea3d9f051fe4e5468b7772b98565e0c
Vipurthanan <lessandsass@gmail.com>
2024-12-26 18:00:37 +0530

    Retrieving data from the database.
    - The public $todos variable was declared in the Listtodos.php component class
    - Then the script was written inside the render function
    - The Todo model was imported above
    - The declared todos variable is available in the view file.
    - The todo mark up was wrapped up by a foreach loop.

--- app/Livewire/Todos/Listtodos.php
+++ app/Livewire/Todos/Listtodos.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire\Todos;
 
+use App\Models\Todo;
 use Livewire\Component;
 
 class Listtodos extends Component
 {
+
+    public $todos;
+
     public function render()
     {
+
+        // $this->todos = Todo::all();
+        $this->todos = Todo::oldest('status')->latest()->get();
+
         return view('livewire.todos.listtodos')->layout('layouts.app');
     }
 }
