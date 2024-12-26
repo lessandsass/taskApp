f0d4c493d97e94bbf8145534ddc332de7fa0e741
Vipurthanan <lessandsass@gmail.com>
2024-12-26 18:07:53 +0530

    single todo component
    - We are going to create the single todo list as a separate livewire component.

---
+++ app/Livewire/Todos/Each.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Todos;
+
+use Livewire\Component;
+
+class Each extends Component
+{
+    public function render()
+    {
+        return view('livewire.todos.each');
+    }
+}
