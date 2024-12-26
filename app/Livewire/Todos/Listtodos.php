9327fccd77aa0df70531785bed27ef0c0ac1bb3b
Vipurthanan <lessandsass@gmail.com>
2024-12-26 12:25:15 +0530

    todo component created.
    - The todo livewire component was created.

---
+++ app/Livewire/Todos/Listtodos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Todos;
+
+use Livewire\Component;
+
+class Listtodos extends Component
+{
+    public function render()
+    {
+        return view('livewire.todos.listtodos');
+    }
+}
