9c9d088dc3f6b23b58efd22ea5f3b7d59c5ce700
Vipurthanan <lessandsass@gmail.com>
2024-12-27 19:36:35 +0530

    ReadMe
    - ReadMe was updated with setup instructions
    - The user_id was added in the databaseSeeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Todo;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,5 +20,10 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        Todo::factory(10)->create([
+            'user_id' => 1,
+        ]);
+
     }
 }
